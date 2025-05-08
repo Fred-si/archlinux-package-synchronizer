951393eb3b19652cb235af832506f93cab5bb3ee
Frédéric <fred-si@zaclys.net>
2025-05-08 16:51:02 +0200

    Feat: create command aps-init

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -3,5 +3,5 @@ from pathlib import Path
 from archlinux_package_synchronizer.config import DEFAULT_CONFIG_DIRECTORY
 
 
-def test_hello():
+def test_hello() -> None:
     assert isinstance(DEFAULT_CONFIG_DIRECTORY, Path)
